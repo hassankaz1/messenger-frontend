c0c90b7ac6c3b39606077a49e8f8110f46b75d37
Hassan Kazi <andaijja@gmail.com>
2023-02-25 17:13:11 -0500

    sidebar chat UI

---
+++ src/components/Cat.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Avatar } from '@mui/material'
+
+
+
+const Cat = () => {
+    return (
+        <Avatar src='https://i.ytimg.com/vi/r7c8hpwiSaA/maxresdefault.jpg' />
+    )
+}
+
+export default Cat
