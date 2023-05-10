eb5c873f9bd1ad6b57b84443270e25ccca7d4734
Hassan Kazi <andaijja@gmail.com>
2023-05-10 16:25:51 -0400

    socket error

--- src/socket.js
+++ src/socket.js
@@ -2,8 +2,11 @@ import io from "socket.io-client";
 
 let socket;
 
+const BASE_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:8000";
+
+
 const connectSocket = (uid) => {
-    socket = io("http://localhost:8000", {
+    socket = io(BASE_URL, {
         query: `uid=${uid}`
     })
 }
